5e187a322655b40f68e183d8fa36814cb2e2ec0f
Ryu-J <71362111+Ryu-J@users.noreply.github.com>
2020-11-16 08:51:19 +0900

    Add files via upload

--- grab_21.py
+++ grab_21.py
@@ -1,10 +1,17 @@
-  
 from __future__ import print_function
 
 import mil as MIL
+import os
+
 
 
 def MGrab():
+    rootdir = "Scanner"
+    try:
+        if not os.path.exists(rootdir):
+            os.makedirs(rootdir)
+    except OSError:
+        print('Error: Creating directory. ' + rootdir)
     # Allocate defaults.
     MilApplication = MIL.MappAlloc(MIL.MIL_TEXT("M_DEFAULT"), MIL.M_DEFAULT, None)
     MilSystem = MIL.MsysAlloc(MIL.M_DEFAULT, MIL.M_SYSTEM_HOST, MIL.M_DEV0, MIL.M_DEFAULT, None)
@@ -13,11 +20,9 @@ def MGrab():
     MilDigitizer_0 = MIL.MdigAlloc(MilSystem, MIL.M_DEV0, MIL.MIL_TEXT("M_DEFAULT"), MIL.M_DEFAULT, None)
     MilDigitizer_1 = MIL.MdigAlloc(MilSystem, MIL.M_DEV1, MIL.MIL_TEXT("M_DEFAULT"), MIL.M_DEFAULT, None)
 
-    SizeBand_0 = MIL.MdigInquire(MilDigitizer_0, MIL.M_SIZE_BAND, None)
     SizeX_0 = MIL.MdigInquire(MilDigitizer_0, MIL.M_SIZE_X, None)
     SizeY_0 = MIL.MdigInquire(MilDigitizer_0, MIL.M_SIZE_Y, None)
 
-    SizeBand_1 = MIL.MdigInquire(MilDigitizer_1, MIL.M_SIZE_BAND, None)
     SizeX_1 = MIL.MdigInquire(MilDigitizer_1, MIL.M_SIZE_X, None)
     SizeY_1 = MIL.MdigInquire(MilDigitizer_1, MIL.M_SIZE_Y, None)
 
@@ -25,9 +30,9 @@ def MGrab():
     SizeX_Par = MIL.MdigInquire(MilDigitizer_0, MIL.M_SIZE_X, None)
     SizeY_Par = MIL.MdigInquire(MilDigitizer_0, MIL.M_SIZE_Y, None)
 
-    MilImageParent = MIL.MbufAllocColor(MilSystem, SizeBand_Par, SizeX_0 + SizeX_1, SizeY_Par, 8 + MIL.M_UNSIGNED, MIL.M_IMAGE + MIL.M_PROC + MIL.M_DISP + MIL.M_GRAB, None)
+    MilImageParent = MIL.MbufAllocColor(MilSystem, SizeBand_Par, SizeX_Par*2, SizeY_Par, 8 + MIL.M_UNSIGNED, MIL.M_IMAGE + MIL.M_PROC + MIL.M_DISP + MIL.M_GRAB, None)
 
-
+    MIL.MbufClear(MilImageParent, MIL.M_COLOR_BLACK)
 
     # MilImageDisp_0 = MIL.MbufAllocColor(MilSystem,
     #                                   SizeBand_0,
@@ -39,7 +44,7 @@ def MGrab():
     #                                   None)
 
     MilImageDisp_0 = MIL.MbufChildColor2d(MilImageParent,
-                                      SizeBand_0,
+                                      MIL.M_ALL_BANDS,
                                       0,0,
                                       SizeX_0,
                                       SizeY_0,
@@ -55,12 +60,12 @@ def MGrab():
     #                                     MIL.M_IMAGE +
     #                                     MIL.M_PROC + MIL.M_DISP + MIL.M_GRAB,
     #                                     None)
-
+    #
     MilImageDisp_1 = MIL.MbufChildColor2d(MilImageParent,
-                                      SizeBand_0,
-                                      2592,0,
-                                      SizeX_0,
-                                      SizeY_0,
+                                      MIL.M_ALL_BANDS,
+                                      SizeX_0,0,
+                                      SizeX_1,
+                                      SizeY_1,
                                       None)
 
     MIL.MbufClear(MilImageDisp_1, MIL.M_COLOR_BLACK)
@@ -79,7 +84,7 @@ def MGrab():
         MIL.MdigHalt(MilDigitizer_1)
         # MIL.MbufExport(MIL.MIL_TEXT("0_FileName{i}.tif".format(i=i)), MIL.M_TIFF, MilImageDisp_0)
         # MIL.MbufExport(MIL.MIL_TEXT("1_FileName{i}.tif".format(i=i)), MIL.M_TIFF, MilImageDisp_1)
-        MIL.MbufExport(MIL.MIL_TEXT("F_FileName{i}.tif".format(i=i)), MIL.M_TIFF, MilImageParent)
+        MIL.MbufExport(MIL.MIL_TEXT("{}/F_FileName{i}.tif".format(rootdir, i=i)), MIL.M_TIFF, MilImageParent)
 
     MIL.MbufFree(MilImageDisp_0)
     MIL.MbufFree(MilImageDisp_1)
